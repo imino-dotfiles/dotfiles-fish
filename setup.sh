2438502c5a8ec27e76891cf36a795573c877c1bd
3u3i <mumiworks@gmail.com>
2020-06-04 20:28:29 +0000

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-: "export"
+set -x
+
+: "export" &&
 {
   SCRIPT_DIR=$(cd $(dirname $0); pwd)
 }
 
-: "symlink"
+: "symlink" &&
 {
   ln -sf $SCRIPT_DIR/fish/config.fish $HOME/.config/fish/config.fish
   ln -sf $SCRIPT_DIR/fish/fish_variables $HOME/.config/fish/fish_variables
